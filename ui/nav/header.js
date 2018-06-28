23eada7777d030224f41984df7ce3d84d26697f8
spacem <spacem@github.com>
2018-06-28 21:57:13 +1000

    add notification text in header

--- ui/nav/header.js
+++ ui/nav/header.js
@@ -2,10 +2,21 @@ angular.module('dnsim').controller('HeaderCtrl',
 [
   function() {
     'use strict';
+
+    var noticeId = 'notice1';
     var vm = this;
+
+    vm.doClose = function() {
+      localStorage.setItem(noticeId, true)
+      vm.isClosed = true;
+    }
+    
+    vm.isClosed = localStorage.getItem(noticeId) != null;
   }
 ]).directive('dngearsimHeader', function() {
   return {
     templateUrl: 'ui/nav/header.html',
+    controller: 'HeaderCtrl',
+    controllerAs: 'ctrl',
   };
 });
