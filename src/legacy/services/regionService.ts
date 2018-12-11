a20ea978516ee1ccb374dbb716439e4691322641
spacem <spacem@github.com>
2018-12-11 20:58:58 +1000

    add br region and update angular to 7.1

--- src/legacy/services/regionService.ts
+++ src/legacy/services/regionService.ts
@@ -14,6 +14,7 @@ function region(translations, dntReset, dntData, $route) {
     // {region: 'jdn', name: 'japan 日本', url : 'https://jdnfiles-59d57.firebaseapp.com'},
     { region: 'cdn', name: 'china 中國', url: 'https://cdnfiles.netlify.com/public' },
     { region: 'kdn', name: 'korea 대한민국', url: 'https://kdnfiles.netlify.com/public' },
+    { region: 'br', name: 'Brazil', url: 'https://dnbr.netlify.com/public' },
   ];
 
   const dntLocationRegion: string = localStorage.getItem('lastDNTRegion');
